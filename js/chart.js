39eabb87e4517db221f51fb3ee2b2a98bac107b0
Severin Kilchhofer <>
2018-11-14 16:45:17 +0100

    added facts (with grid but useless now) added chart added media queries

--- js/chart.js
+++ js/chart.js
@@ -51,8 +51,12 @@ Highcharts.chart('container', {
             text: null
         },
         labels: {
+            style: {
+                color: '#FFFFFF',
+                fontWeight: 'bold',
+            },
             formatter: function () {
-                return Math.abs(this.value) + '%';
+                return Math.abs(this.value) + '/km';
             }
         }
     },
@@ -69,17 +73,25 @@ Highcharts.chart('container', {
         }
     },
 
+    exporting: {
+        enabled: false
+    },
 
-    borderColor: ['transparent'],
 
-    colors: ['#F2C314', '#FC3832'],
+    // colors: ['#F2C314', '#FC3832'],
 
 
     series: [{
         name: 'Severin',
         data: [
             -4.06, -4.12, -4.11, -4.14, -4.17, -3.51, -4.05, -4.02, -4.06, -3.56, -3.57, -4.00, -3.46, -4.15, -3.52, -4.03, -4.05, -3.54, -3.56, -3.50, -3.40,
-        ]
+        ],
+        borderColor: 'transparent',
+        color: {linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+            stops: [
+                [0, '#FC3832'],
+                [1, '#F2C314']
+            ]}
     }, {
         name: 'Kevin',
         data: [
@@ -88,6 +100,12 @@ Highcharts.chart('container', {
             4.3, 4.0, 3.5, 2.9, 2.5,
             2.7, 2.2, 1.1, 0.6, 0.2,
             0.0
-        ]
+        ],
+        borderColor: 'transparent',
+        color: {linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+            stops: [
+                [0, '#32FBFC'],
+                [1, '#3214F2']
+            ]}
     }]
 });
